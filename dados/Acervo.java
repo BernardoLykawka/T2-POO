24cf06c1ea65ec077551bb4e0c46fe14e7976d61
Bernardo Lykawka <bernardolmsilvas@gmail.com>
2024-10-22 11:13:13 -0300

    add Classes

---
+++ dados/Acervo.java
@@ -0,0 +1,11 @@
+package dados;
+
+public class Acervo {
+
+	private ArrayList<Video> video;
+
+	public boolean addVideo(Video v) {
+		return false;
+	}
+
+}
---
+++ dados/Video.java
@@ -0,0 +1,29 @@
+package dados;
+
+public abstract class Video implements Imprimivel {
+
+	private int codigo;
+
+	private String titulo;
+
+	public Video(int codigo, String titulo) {
+
+	}
+
+
+	/**
+	 * @see dados.Imprimivel#geraTexto()
+	 */
+	public String geraTexto() {
+		return null;
+	}
+
+
+	/**
+	 * @see dados.Imprimivel#calculaCusto()
+	 */
+	public double calculaCusto() {
+		return 0;
+	}
+
+}
